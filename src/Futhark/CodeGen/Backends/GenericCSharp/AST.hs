a0d0f1f2b33931e23f09cded33ed4762528441b3
Mikkel Storgaard Knudsen <mikkelstorgaard@gmail.com>
2018-02-23 13:01:59 +0100

    asd

--- src/Futhark/CodeGen/Backends/GenericCSharp/AST.hs
+++ src/Futhark/CodeGen/Backends/GenericCSharp/AST.hs
@@ -14,6 +14,44 @@ module Futhark.CodeGen.Backends.GenericCSharp.AST
 import Language.Futhark.Core
 import Futhark.Util.Pretty
 
+data MemType = Reference
+             | Pointer
+             deriving (Eq, Show)
+
+data ArgMemType = ArgOut
+                | ArgRef
+                deriving (Eq, Show)
+
+data CompType = ListT CSharpType
+              | ArrayT CSharpType
+              | TupleT [CSharpType]
+              deriving (Eq, Show)
+
+data DataType = CompType
+              | IntT
+              | DoubleT
+              | FloatT
+              | StringT
+              | CustomT String
+-- dunno whether to fill on more of the necessary types yet,
+-- but that can be done eventually
+              deriving (Eq, Show)
+
+data PrivacyType = Blank
+                 | Public
+                 | Private
+                 | Protected
+                 | Internal
+                 deriving (Eq, Show)
+
+data Modifier = Const
+              | Readonly
+              | Static
+              deriving (Eq, Show)
+
+data CSharpType = CSharpType (Maybe MemType) DataType
+                deriving (Eq, Show)
+
 
 data UnOp = Not -- ^ Boolean negation.
           | Complement -- ^ Bitwise complement.
@@ -37,5 +75,57 @@ data CSharpExp = Integer Integer
            | List [CSharpExp]
            | Field CSharpExp String
            | Dict [(CSharpExp, CSharpExp)]
-           | None
+           | Null
              deriving (Eq, Show)
+
+data CSharpIdx = IdxRange CSharpExp CSharpExp
+               | IdxExp CSharpExp
+               deriving (Eq, Show)
+
+
+data CSharpArg = ArgKeyword String CSharpArg'
+               | Arg CSharpArg'
+               deriving (Eq, Show)
+
+data CSharpArg' = Arg' (Maybe ArgMemType) (Maybe MemType) CSharpExp
+                deriving (Eq, Show)
+
+
+data CSharpStmt = If CSharpExp [CSharpStmt] [CSharpStmt]
+                | Try [CSharpStmt] [CSharpExcept]
+                | While CSharpExp [CSharpStmt]
+                | For String CSharpExp [CSharpStmt]
+                | With CSharpExp [CSharpStmt]
+                | Unsafe [CSharpStmt]
+
+                -- Maybe declare type (instead of just assigning a 'var'), and
+                -- maybe cast assignment
+                | Assign PrivacyType [Modifier] (Maybe CSharpType) CSharpExp
+                                                (Maybe CSharpType) CSharpExp
+
+                | AssignOp String CSharpExp CSharpExp
+                | Comment String [CSharpStmt]
+                | Assert CSharpExp String
+                | Raise CSharpExp
+                | Exp CSharpExp
+                | Return CSharpExp
+                | Pass
+
+                -- Definition-like statements.
+                | Import String (Maybe String)
+                | FunDef CSharpFunDef
+                | ClassDef CSharpClassDef
+
+                -- Some arbitrary string of CSharp code.
+                | Escape String
+                deriving (Eq, Show)
+
+data CSharpExcept = Catch CSharpExp [CSharpStmt]
+              deriving (Eq, Show)
+
+type CSharpFunDefArg = (Maybe ArgMemType, CSharpType, String)
+data CSharpFunDef = Def String PrivacyType [Modifier] [CSharpFunDefArg] [CSharpStmt]
+                  deriving (Eq, Show)
+
+data CSharpClassDef = Class PrivacyType [Modifier] [CSharpStmt]
+                deriving (Eq, Show)
