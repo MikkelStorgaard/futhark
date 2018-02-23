55906cf62776e047c1607564a65dcfbdfc40074d
Mikkel Storgaard Knudsen <mikkelstorgaard@gmail.com>
2018-02-23 15:57:23 +0100

    beep doot

--- src/Futhark/CodeGen/Backends/GenericCSharp/AST.hs
+++ src/Futhark/CodeGen/Backends/GenericCSharp/AST.hs
@@ -14,28 +14,50 @@ module Futhark.CodeGen.Backends.GenericCSharp.AST
 import Language.Futhark.Core
 import Futhark.Util.Pretty
 
-data MemType = Reference
-             | Pointer
-             deriving (Eq, Show)
+data MemT = Pointer
+          deriving (Eq, Show)
 
 data ArgMemType = ArgOut
                 | ArgRef
                 deriving (Eq, Show)
+
+instance Pretty ArgMemType where
+  ppr ArgOut = text "out"
+  ppr ArgRef = text "ref"
 
 data CompType = ListT CSharpType
               | ArrayT CSharpType
               | TupleT [CSharpType]
               deriving (Eq, Show)
 
-data DataType = CompType
-              | IntT
-              | DoubleT
-              | FloatT
-              | StringT
-              | CustomT String
--- dunno whether to fill on more of the necessary types yet,
--- but that can be done eventually
-              deriving (Eq, Show)
+instance Pretty CompType where
+  ppr (ArrayT t) = ppr t <> text "[]"
+  ppr (TupleT ts) = text "Tuple" <> angles $ commasep $ map ppr ts
+
+data BaseT = IntT
+           | DoubleT
+           | FloatT
+           | StringT
+           | CustomT String
+           -- dunno whether to fill on more of the necessary types yet,
+           -- but that can be done eventually
+
+instance Pretty BaseT where
+  ppr IntT = text "int"
+  ppr DoubleT = text "double"
+  ppr FloatT = text "float"
+  ppr StringT = text "string"
+  ppr (CustomT s) = text s
+
+data CSharpType = CompositeType CompT
+                | PointerType CSharpType
+                | BaseType BaseT
+                deriving (Eq, Show)
+
+instance Pretty CSharpType where
+  ppr (CompositeType t) = ppr t
+  ppr (PointerType t) = parens $ ppr t <> text "*"
+  ppr (BaseType t) = ppr t
 
 data PrivacyType = Blank
                  | Public
@@ -44,14 +66,22 @@ data PrivacyType = Blank
                  | Internal
                  deriving (Eq, Show)
 
+instance Pretty PrivacyType where
+  ppr Blank = text "blank"
+  ppr Public = text "public"
+  ppr Private = text "private"
+  ppr Protected = text "protected"
+  ppr Internal = text "internal"
+
 data Modifier = Const
               | Readonly
               | Static
               deriving (Eq, Show)
 
-data CSharpType = CSharpType (Maybe MemType) DataType
-                deriving (Eq, Show)
-
+instance Pretty Modifier where
+  ppr Const = text "const"
+  ppr Readonly = text "readonly"
+  ppr Static = text "static"
 
 data UnOp = Not -- ^ Boolean negation.
           | Complement -- ^ Bitwise complement.
@@ -65,6 +95,7 @@ data CSharpExp = Integer Integer
            | String String
            | RawStringLiteral String
            | Var String
+           | Ref MemT String
            | BinOp String CSharpExp CSharpExp
            | UnOp String CSharpExp
            | Cond CSharpExp CSharpExp CSharpExp
@@ -72,11 +103,22 @@ data CSharpExp = Integer Integer
            | Call CSharpExp [CSharpArg]
            | Cast CSharpExp String
            | Tuple [CSharpExp]
-           | List [CSharpExp]
+           | Array [CSharpExp]
            | Field CSharpExp String
            | Dict [(CSharpExp, CSharpExp)]
            | Null
              deriving (Eq, Show)
+
+instance Pretty CSharpExp where
+  ppr (Integer x) = ppr x
+  ppr (Bool x) = text $ (toLower . show) b
+  ppr (Float x)
+    | isInfinite x = text $ if x > 0 then "Double.PositiveInfinity" else "Double.NegativeInfinity"
+    | otherwise = ppr x
+  ppr (String x) = text $ show x
+  ppr (RawStringLiteral s)
+
+
 
 data CSharpIdx = IdxRange CSharpExp CSharpExp
                | IdxExp CSharpExp
